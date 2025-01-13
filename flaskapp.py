0f13d5a8d64939fce91850378f1ad4650430da04
Pradeep Rawat <pradeep76delhi@gmail.com>
2025-01-13 20:30:08 +0530

    modified host

--- flaskapp.py
+++ flaskapp.py
@@ -109,4 +109,4 @@ def number_operations():
     return render_template_string(html_code, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
